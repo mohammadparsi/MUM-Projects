33308526e2c16dc70cecabd4eda0247a853dc892
Unknown <mohammadparsi@rocketmail.com>
2017-09-21 16:50:39 -0500

    sds

--- Lab11/src/app/app.module.ts
+++ Lab11/src/app/app.module.ts
@@ -3,19 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
-import { UnorderedListComponent } from './unordered-list/unordered-list.component';
-import { UpperDirective } from './upper.directive';
-import { MyVisibilityDirective } from './my-visibility.directive';
-import { MycolorDirective } from './mycolor.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    UnorderedListComponent,
-    UpperDirective,
-    MyVisibilityDirective,
-    MycolorDirective
+    CounterComponent
   ],
   imports: [
     BrowserModule
